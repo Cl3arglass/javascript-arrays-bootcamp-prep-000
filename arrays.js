7567a2d7e18760ee9e3cbe9485b41ead4f494fc2
Holden Altaffer <haltaffer@gmail.com>
2017-10-30 22:17:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,6 +40,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 };
 
 function removeElementFromEndOfArray(array) {
-  array.slice(1, array.length - 2);
+  array.slice(2);
   return array
 }
